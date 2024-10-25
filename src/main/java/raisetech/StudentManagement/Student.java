4fe17e9f078a348500d3aad3f745f1a17e5cdcf0
stride6 <miyamutmusasasi@gmail.com>
2024-10-25 12:00:36 +0900

    Merge remote-tracking branch 'origin/スタンダード課題5' into スタンダード課題5
    スタンダード5追加修正(10月12日)

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,9 +1,25 @@
 package raisetech.StudentManagement;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import lombok.Getter;
-import lombok.Setter;
+public class Student {
 
-@JsonAnyGetter
+    private String name;
+    private int age;
 
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age){
+
+        this.age = age;
+    }
+
+}
