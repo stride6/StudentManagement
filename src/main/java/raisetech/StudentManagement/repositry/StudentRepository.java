99db62874bae6853276aec5877eded84145d3c6e
stride6 <miyamutmusasasi@gmail.com>
2024-11-04 21:42:13 +0900

    Merge remote-tracking branch 'origin/スタンダード課題5' into スタンダード課題5
    スタンダード5追加修正(10月12日)

--- src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
@@ -2,6 +2,7 @@ package raisetech.StudentManagement.repositry;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.StudentsCourses;
 
