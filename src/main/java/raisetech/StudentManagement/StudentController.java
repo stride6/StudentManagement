58e8821e358848971a18dbd835e51ca1c8cd61d9
stride6 <miyamutmusasasi@gmail.com>
2024-11-21 00:43:51 +0900

    Merge remote-tracking branch 'origin/スタンダード課題5' into スタンダード課題5
    スタンダード5追加修正(10月12日)

--- src/main/java/raisetech/StudentManagement/StudentController.java
+++ src/main/java/raisetech/StudentManagement/StudentController.java
@@ -2,13 +2,32 @@ package raisetech.StudentManagement;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 private StudentService service;
 
 @Autowired
-public  StudentController()
+public  StudentController(StudentService service) {
+this.service.searchStudentList();
+}
+
+@GetMapping("/studentList")
+public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    service.searchStudentCourseList();
+    return
+}
+
+@GetMapping("/studentsCourseList")
+public List<StudentsCourses> getStudentsCourseList()　{
+return service.searchStudentCourseList();
+}
 }
