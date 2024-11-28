b246a626aab663fd60a20aed6972baeb1e9b7a8b
stride6 <miyamutmusasasi@outlook.com>
2024-11-28 19:13:49 +0900

    Update ServletInitializer.java

--- src/main/java/raisetech/StudentManagement/ServletInitializer.java
+++ src/main/java/raisetech/StudentManagement/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(StudentmanagementApplication.class);
+		return application.sources(StudentManagementApplication.class);
 	}
 
 }
