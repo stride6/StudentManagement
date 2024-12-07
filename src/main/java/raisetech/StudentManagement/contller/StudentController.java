86b0fccd9fce80716105173db115bd8d17a08349
stride6 <miyamutmusasasi@gmail.com>
2024-12-07 13:01:24 +0900

    Merge remote-tracking branch 'origin/スタンダード課題5' into スタンダード課題5
    スタンダード5追加修正(10月12日)

--- src/main/java/raisetech/StudentManagement/contller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/contller/StudentController.java
@@ -4,10 +4,14 @@ package raisetech.StudentManagement.contller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.StudentManagement.contller.converter.Studentconverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 import java.util.ArrayList;
@@ -43,5 +47,18 @@ public class StudentController {
         return service.searchStudentsCourseList();
     }
 
+@GetMapping("/newStudent")
+public String newStudent(Model model) {
+model.addAttribute("studentDetail", new StudentDetail());
+return "registerStudent";
+}
 
+@PostMapping("/registerStudent")
+public String  registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
+        if(result.hasErrors()) {
+return "registerStudent";
+        }
+//System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+        return "redirect:/studentList";
+}
 }
