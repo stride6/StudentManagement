911c0ecf75106bdd68accce9de36d14f65913e48
stride6 <miyamutmusasasi@gmail.com>
2025-01-18 12:01:44 +0900

    スタンダード20とマージ

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,51 +1,15 @@
 package raisetech.StudentManagement;
 
-//import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Select;
 
-//import org.apache.ibatis.annotations.Select;
-
-import org.apache.ibatis.annotations.*;
-
-//import java.util.List;
-
-import org.apache.ibatis.annotations.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class StudentManagementApplication {
+    public static void main(String[] args) {
 
-	@Autowired
-	private StudentRepository repository;
-
-	public static void main(String[] args) {
-
-    @GetMapping("/studentList")
-    public List<Student> getStudentList () {
-        return repository.search();
+        SpringApplication.run(StudentManagementApplication.class, args);
     }
 }
 
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-		
-	}
 
-	@PatchMapping("/Student")
-			public void updateStudent(String name, int age){
-		repository.updateStudent(name, age);
-	}
-
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
-}
