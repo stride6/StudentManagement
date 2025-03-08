f758968b437c8e983fec1d0a5fcae08650761317
stride6 <miyamutmusasasi@gmail.com>
2025-03-08 15:39:19 +0900

    スタンダード課題25の確認をお願いします。

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -84,6 +84,8 @@ public class StudentController {
         return ResponseEntity.ok("更新処理が成功しました。");
     }
 
+
+
     @ExceptionHandler(TestException.class)
     public ResponseEntity<String> handleTestException(TestException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
