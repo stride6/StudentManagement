e9928d770fd6c89817ea5688a2c979031142f78c
stride6 <miyamutmusasasi@gmail.com>
2025-04-16 10:46:07 +0900

    スタンダード課題27の確認をお願いします。

--- src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
+++ src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
@@ -32,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                     .andExpect(content().json("{}"));
 verify(service, times(1)).searchStudentList();
         }
+
     }
 
 
