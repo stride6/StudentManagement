0f54b4e63a56d27af1608cfaed1e9b5b29576867
stride6 <miyamutmusasasi@gmail.com>
2025-04-17 10:17:35 +0900

    スタンダード課題27の確認をお願いします。

--- src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
@@ -40,7 +40,7 @@ public interface StudentRepository {
     /**
      * 受講生の検索を行います。
      *
-     * @param id　受講生ID
+     * @param id 　受講生ID
      * @return 受講生
      */
     @Select("SELECT * FROM students WHERE id = #{id}")
@@ -57,7 +57,7 @@ public interface StudentRepository {
     /**
      * 受講生IDに紐付く受講生コース情報を検索します。
      *
-     * @param studentId　受講生ID
+     * @param studentId 　受講生ID
      * @return 受講生IDに紐付く受講生コース情報
      */
     @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
@@ -66,7 +66,7 @@ public interface StudentRepository {
     /**
      * 受講生情報を新規登録します。IDに関しては自動採番を行う。
      *
-     * @param student　受講生
+     * @param student 　受講生
      */
     @Insert("INSERT INTO students(name,kana_name,nickname,email,area,age,sex,remark,is_deleted) " +
             "VALUES(#{name}, #{kanaName}, #{nickname},#{email}, #{area},#{age},#{sex},#{remark},false)")
@@ -76,7 +76,7 @@ public interface StudentRepository {
     /**
      * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
      *
-     * @param studentCourse　受講生コース情報
+     * @param studentCourse 　受講生コース情報
      */
     @Insert("INSERT INTO students_courses(student_id,course_name,course_start_at,course_end_at) "
             + "VALUES(#{studentId}, #{courseName},#{courseStartAt},#{courseEndAt})")
@@ -86,7 +86,7 @@ public interface StudentRepository {
     /**
      * 受講生を更新します。
      *
-     * @param student　受講生
+     * @param student 　受講生
      */
     @Update("UPDATE students SET name = #{name}, kana_name= #{kanaName}, nickname = #{nickname},"
             + "email = #{email}, area = #{area},age = #{age},sex = #{sex},remark = #{remark},is_deleted = #{isDeleted} WHERE id = #{id}")
@@ -95,7 +95,7 @@ public interface StudentRepository {
     /**
      * 受講生コース情報のコース名を更新します。
      *
-     * @param studentCourse　受講生コース情報
+     * @param studentCourse 　受講生コース情報
      */
     @Update("UPDATE students_courses SET course_name =#{courseName}) WHERE id = #{id}")
     void updateStudentCourse(StudentCourse studentCourse);
