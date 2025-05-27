f9969d49c49c5293f4fd94130917f17b2d1cc8b0
stride6 <miyamutmusasasi@gmail.com>
2025-05-27 13:18:57 +0900

    マージ修正完了。

--- src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repositry/StudentRepository.java
@@ -34,14 +34,13 @@ public interface StudentRepository {
 
 
     //    @Select("students")
-
-//    @Select("SELECT * FROM students")
-    List<Student>search();
+    @Select("SELECT * FROM students")
+    List<Student> search();
 
     /**
      * 受講生の検索を行います。
      *
-     * @param id　受講生ID
+     * @param id 　受講生ID
      * @return 受講生
      */
     Student searchStudent(String id);
@@ -51,20 +50,21 @@ public interface StudentRepository {
      *
      * @return　受講生のコース情報(全件)
      */
+    @Select("SELECT * FROM students_courses")
     List<StudentCourse> searchStudentCoursesList();
 
     /**
      * 受講生IDに紐付く受講生コース情報を検索します。
      *
-     * @param studentId　受講生ID
+     * @param studentId 　受講生ID
      * @return 受講生IDに紐付く受講生コース情報
      */
-        List<StudentCourse> searchStudentCourse(String studentId);
+    List<StudentCourse> searchStudentCourse(String studentId);
 
     /**
      * 受講生情報を新規登録します。IDに関しては自動採番を行う。
      *
-     * @param student　受講生
+     * @param student 　受講生
      */
     @Insert("INSERT INTO students(name,kana_name,nickname,email,area,age,sex,remark,is_deleted) " +
             "VALUES(#{name}, #{kanaName}, #{nickname},#{email}, #{area},#{age},#{sex},#{remark},false)")
@@ -74,7 +74,7 @@ public interface StudentRepository {
     /**
      * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
      *
-     * @param studentCourse　受講生コース情報
+     * @param studentCourse 　受講生コース情報
      */
     @Insert("INSERT INTO students_courses(student_id,course_name,course_start_at,course_end_at) "
             + "VALUES(#{studentId}, #{courseName},#{courseStartAt},#{courseEndAt})")
@@ -84,7 +84,7 @@ public interface StudentRepository {
     /**
      * 受講生を更新します。
      *
-     * @param student　受講生
+     * @param student 　受講生
      */
     @Update("UPDATE students SET name = #{name}, kana_name= #{kanaName}, nickname = #{nickname},"
             + "email = #{email}, area = #{area},age = #{age},sex = #{sex},remark = #{remark},is_deleted = #{isDeleted} WHERE id = #{id}")
@@ -93,7 +93,7 @@ public interface StudentRepository {
     /**
      * 受講生コース情報のコース名を更新します。
      *
-     * @param studentCourse　受講生コース情報
+     * @param studentCourse 　受講生コース情報
      */
     @Update("UPDATE students_courses SET course_name =#{courseName} WHERE id = #{id}")
     void updateStudentCourse(StudentCourse studentCourse);
